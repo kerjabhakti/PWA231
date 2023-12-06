b233ac584fa063c947567779717814311908a7e5
Rakawilangga30 <rakawilangga713@gmail.com>
2023-12-06 23:04:52 +0700

    substr

---
+++ Chapter08/README.md/TugasPHP/714230023_AnandaRaka/substr.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>fungsi : String</title>
+</head>
+<body>
+<h2>fungsi : String substr()</h2>
+<?php 
+$string="Pemrograman Web PHP";
+$word=substr($string,16,3);
+echo("<1>\"$string\"</1> <br>");
+echo("Hasil substr (15,3) adalah $word");
+?>
+</body>
+</html>
