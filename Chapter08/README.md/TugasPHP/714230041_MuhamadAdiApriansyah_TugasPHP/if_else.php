cb9bc62daa6f99b6ab6610b2bffaad881ddefaa4
owel17 <onicadi17@gmail.com>
2023-12-06 23:56:20 +0700

    ini tugas chapter 8 adi

---
+++ Chapter08/README.md/TugasPHP/714230041_MuhamadAdiApriansyah_TugasPHP/if_else.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>if</title>
+</head>
+<body>
+<h2>Penggunaan Kontrol if</h2>
+<?php
+    $umur=15;
+    echo("Umur anda $umur tahun <br>");
+    if($umur>=17)
+    {
+        echo("Anda boleh masuk");
+    }else
+    
+    {
+        echo("Anda tidak boleh masuk");
+    
+    }
+?>
+</body>
+</html>
