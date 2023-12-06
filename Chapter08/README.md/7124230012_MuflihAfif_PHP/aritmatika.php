c2dc5f5c1901b244be2b46bc15e646b31a059110
Muflih Afif <muflihafif234@gmail.com>
2023-12-06 22:27:30 +0700

    Tugas PHP MUflih

---
+++ Chapter08/README.md/7124230012_MuflihAfif_PHP/aritmatika.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Aritmatika</h2>
+    <?php
+        $a = 1;
+        $b = 3;
+        $c = 5;
+        $s1 = "Hallo" ;
+            print ("Penjumlahan dan pengurangan :<br> \n");
+            printf ("$a + $b = %d <br> \n", $a + $b);
+            printf ("$a - $b = %d <br> \n", $a - $b);
+            print ("<br> Perkalian : <br \n>");
+            printf ("$c * $b = %d <br> \n", $c * $b);
+            printf ("$c * 0.5 = %d <br> \n", $c * 0.5);
+            print ("<br> Pembagian :<br> \n");
+            printf ("$c / $a = %d <br> \n", $c / $a);
+            printf ("$c / 5.5 = %d <br> \n", $c / 5.5);
+            printf ("<br> Yang ini menampilan variabel String = $s1");
+    ?>
+</body>
+</html>
