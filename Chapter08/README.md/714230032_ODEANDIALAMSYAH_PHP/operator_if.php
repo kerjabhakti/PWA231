670dc00fbe7a57e72aa6025b6ec9ac968af337d4
odeandialamsyah <odeandialamsyah@gmail.com>
2023-12-06 23:49:29 +0700

    praktik operator if

---
+++ Chapter08/README.md/714230032_ODEANDIALAMSYAH_PHP/operator_if.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>if</title>
+    </head>
+    <body>
+        <h2>penggunakan kontrol if</h2>
+        <?php
+        $umur=15;
+        echo("usia mobil anda adalah $umur tahun dan 20 tahun adalah batasan penggunaan"."<br>");
+        if($umur=15)
+        {
+            echo("belum melewati batasan");
+        }
+       
+        ?>
+    </body>
+</html>
