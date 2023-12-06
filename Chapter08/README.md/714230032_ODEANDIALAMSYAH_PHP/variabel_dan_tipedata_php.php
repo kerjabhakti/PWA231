13a4fc6435d6c812e7d308f845864868333af2ec
odeandialamsyah <odeandialamsyah@gmail.com>
2023-12-06 23:53:46 +0700

    praktik variabel dan tipe data

---
+++ Chapter08/README.md/714230032_ODEANDIALAMSYAH_PHP/variabel_dan_tipedata_php.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>variabel dan tipe data php</title>
+</head>
+<body>
+    <h2>variabel dan tipe data php</h2>
+    <?php
+    $var1="nama";
+    $var2=2;
+    $var3=3.14;
+    echo("\$var1=$var1<br>");
+    echo("\$var2=$var2<br>");
+    echo("\$var3=$var3<br>");
+    echo("tipe data \$var1 adalah ".gettype($var1)."<br>");
+    echo("tipe data \$var2 adalah ".gettype($var2)."<br>");
+    echo("tipe data \$var3 adalah ".gettype($var3)."<br>");
+
+    ?>    
+</body>
+</html>
