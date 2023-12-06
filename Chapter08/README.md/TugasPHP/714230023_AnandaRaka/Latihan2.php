8414446dbe94094f679ba2bcbe0a04811dcec212
Rakawilangga30 <rakawilangga713@gmail.com>
2023-12-06 23:00:08 +0700

    latihan 2

---
+++ Chapter08/README.md/TugasPHP/714230023_AnandaRaka/Latihan2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Latihan 3</title>
+</head>
+<body>
+<h2>Jumlah bilangan genap 0->6</h2>
+<?php
+$jumlah = 0;
+for($i = 0; $i <= 6; $i += 2)
+{
+    $jumlah += $i;
+}
+echo "Jumlah bilangan genap dari 0 sampai 6 adalah $jumlah";
+?>
+</body>
+</html>
