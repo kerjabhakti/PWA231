c27cf16f62d28690fc8ec45d66633ff35fab6472
odeandialamsyah <odeandialamsyah@gmail.com>
2023-12-06 23:53:09 +0700

    praktik ucfirst

---
+++ Chapter08/README.md/714230032_ODEANDIALAMSYAH_PHP/ucfirst.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>ucfirst</title>
+    </head>
+    <body>
+        <h2>hurus pertama kapital pada string (ucfirst)</h2>
+        <?php
+        $string=("pemprograman web php");
+        $word=ucfirst($string);
+        echo("<i>\"$string\"</i> <br>");
+        echo("hasil ucfirst adalah <i>\"$word\"<i>");
+        ?>
+    </body>
+</html>
