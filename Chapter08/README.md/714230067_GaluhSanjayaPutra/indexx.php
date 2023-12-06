0706f8793c71ace0c53aa0b95d452cc6d859bf80
galuh putra <galuhputra1308@gmail.com>
2023-12-06 22:07:53 +0700

    Praktikum

---
+++ Chapter08/README.md/714230067_GaluhSanjayaPutra/indexx.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = "Galuh Sanjaya Putra"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Hello, Selamat datang <?php echo "$nama" ?></h1>
+</body>
+</html>
