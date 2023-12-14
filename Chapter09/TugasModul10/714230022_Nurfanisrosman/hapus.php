524886f04ee318cf3391c6a8f7ca016292912dfb
Rosmanroot <nurfanisrosmanroot@gmail.com>
2023-12-14 08:10:33 +0700

    nambahin file yang hapus

---
+++ Chapter09/TugasModul10/714230022_Nurfanisrosman/hapus.php
@@ -0,0 +1,25 @@
+<?php
+
+include("config.php");
+
+if( isset($_GET['id']) ){
+
+    
+    $id = $_GET['id'];
+
+   
+    $sql = "DELETE FROM pendaftaran WHERE id=$id";
+    $query = mysqli_query($db, $sql);
+
+  
+    if( $query ){
+        header('Location: list-maba.php');
+    } else {
+        die("gagal menghapus...");
+    }
+
+} else {
+    die("akses dilarang...");
+}
+
+?>
